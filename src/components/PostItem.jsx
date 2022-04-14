dc9a96712301152aeb6a52cb2e8d4f32b02804f5
Sergey Shershnev <SergeyS@MacBook-Pro.local>
2022-04-14 14:57:56 +0300

    v2

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import MyButton from "./UI/button/MyButton";
+import {useNavigate} from "react-router-dom";
 
 const PostItem = (props) => {
+
+    const navigate = useNavigate()
 
     return (
         <div className="post">
@@ -11,7 +14,11 @@ const PostItem = (props) => {
                     {props.post.body}
                 </div>
             </div>
-            <div className="post_btns">
+
+            <div className="post__btns">
+                <MyButton onClick={() => navigate("/posts/" + props.post.id)}>
+                    Open
+                </MyButton>
                 <MyButton onClick={() => props.remove(props.post)}>
                     Delete
                 </MyButton>
